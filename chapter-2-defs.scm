aaa9b533e6ea4f1a35ad29d95ef43ef38b0102ce
Renato Fernandes de Queiroz <renatofq@gmail.com>
2023-08-06 09:40:09 -0300

    Exercises 2.1, 2.2 and 2.3

---
+++ chapter-2-defs.scm
@@ -0,0 +1,39 @@
+;; math functions
+(define (identity a) a)
+(define (inc n) (+ n 1))
+(define (dec n) (- n 1))
+(define (square x) (* x x))
+(define (cube x) (* x x x))
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+;; test functions
+(define (display-test-fail expected actual)
+  (display "fail! expected: ")
+  (display expected)
+  (display "; actual: ")
+  (display actual)
+  (display ";")
+  (newline))
+
+(define (test-assert expected)
+  (if (not expected)
+      (display-test-fail expected actual)))
+
+
+(define (test-equal expected actual)
+  (if (not (= expected actual))
+      (display-test-fail expected actual)))
+
+(define (test-approximate expected actual error)
+  (if (< error
+         (abs (- expected actual)))
+      (display-test-fail expected actual)))
