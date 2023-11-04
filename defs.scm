23c4661d963993ffc68c786fa19ffcc50838e2d7
Renato Fernandes de Queiroz <renatofq@gmail.com>
2023-11-04 07:11:42 -0300

    fix defs.scm, start of section 2.3

--- defs.scm
+++ defs.scm
@@ -12,20 +12,14 @@
 (define (divides? a b)
   (= (remainder b a) 0))
 
-(define (gcd a b)
-  (if (= b 0)
-      a
-      (gcd b (remainder a b))))
-
 (define (smallest-divisor n)
   (define (find-divisor n test-divisor)
     (cond ((> (square test-divisor) n)
            n)
           ((divides? test-divisor n)
            test-divisor)
-          (else (find-uneven-divisor
-                 n
-                 (+ test-divisor 2)))))
+          (else
+           (find-divisor n (+ test-divisor 2)))))
   (if (divides? 2 n)
       2
       (find-divisor n 3)))
