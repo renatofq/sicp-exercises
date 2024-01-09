c5c62641547cb0732bdbf486a9311b0a8f63a813
Renato Fernandes de Queiroz <renatofq@gmail.com>
2024-01-09 19:02:19 -0300

    3.72 to 3.80 left as TODO

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,16 +4,12 @@
              (let* ((dir (dir-locals-find-file "."))
                     (full-path (expand-file-name
                                 "defs.scm"
-                                (if (stringp dir) dir (car dir))))
-                    ;;(forms (format "(load \"%s\")" full-path))
-                    )
-               ;; (set 'geiser-repl-startup-forms (list forms))
+                                (if (stringp dir) dir (car dir)))))
                (lambda () (geiser-load-file full-path)))))))
  (org-mode . ((mode . flyspell)
               (mode . olivetti)
               (ispell-local-dictionary . "american")
               (org-export-with-toc . nil)
-              ;; (org-export-with-todo-keywords . nil)
               (org-export-with-section-numbers . nil)
               (org-html-head . "<style type=\"text/css\">.org-svg { max-width: 100%; }</style>")
               (org-global-properties . ((header-args . ":results output")))
